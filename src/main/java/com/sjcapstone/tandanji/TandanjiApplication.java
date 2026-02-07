d5e5258988af3bac5ad0c60db17d2a4dd5d3b608
Sohee <msciki790@gmail.com>
2026-02-07 20:41:22 +0900

    init settings

---
+++ src/main/java/com/sjcapstone/tandanji/TandanjiApplication.java
@@ -0,0 +1,13 @@
+package com.sjcapstone.tandanji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TandanjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TandanjiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sjcapstone/tandanji/TandanjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjcapstone.tandanji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TandanjiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
