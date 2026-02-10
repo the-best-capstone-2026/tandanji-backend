832cb9103db75c60b4ae8a6ccd67a1df864fa700
Dongju <dongju@tadanji.com>
2026-02-10 15:46:57 +0900

    feat: 이미지 업로드 확인용 컨트룰러 구현 완료

---
+++ src/main/java/com/sjcapstone/tandanji/controller/PredictionController.java
@@ -0,0 +1,15 @@
+package com.sjcapstone.tandanji.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+@RestController
+
+public class PredictionController {
+    @PostMapping("/api/predict")
+    public String predict(@RequestParam("image") MultipartFile image) {
+        System.out.println("받은 파일 이름: " + image.getOriginalFilename());
+        return "이미지 수신 성공!";
+    }
+}
